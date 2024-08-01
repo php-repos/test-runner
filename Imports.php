00f35f0d51f0421eef34d9cc3633c5409a5dd59a
Morteza Poussaneh <morteza@protonmail.com>
2024-08-01 14:44:32 +0200

    Autoload projects import file on each run

--- Imports.php
+++ Imports.php
@@ -1,5 +1,7 @@
 <?php
 
+$phpkg_import_file = getenv('PHPKG_IMPORT_FILE');
+require_once "$phpkg_import_file";
 require_once __DIR__ . '/Source/Assertions.php';
 require_once __DIR__ . '/Source/Statistics.php';
 require_once __DIR__ . '/Source/Runner.php';
